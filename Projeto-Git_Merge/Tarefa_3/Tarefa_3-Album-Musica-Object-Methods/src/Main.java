104fa94d996a61787f7ed1320d5aa72dec0efd7b
DiegoASCW <diegolautens@outlook.com>
2024-03-13 21:13:26 -0300

    Update

--- Projeto-Git_Merge/Tarefa_3/Tarefa_3-Album-Musica-Object-Methods/src/Main.java
+++ Projeto-Git_Merge/Tarefa_3/Tarefa_3-Album-Musica-Object-Methods/src/Main.java
@@ -5,11 +5,13 @@ public class Main{
     public static void main(String[] args) {
 
         List<Musica> playlist = new ArrayList<Musica>();
-        playlist.add(new Musica("COBOLÍSTICO"));
-        playlist.add(new Musica("C#!"));
-        playlist.add(new Musica("Javai"));
+        playlist.add(new Musica("C+"));
+        playlist.add(new Musica("C++"));
+        playlist.add(new Musica("C++++"));
+        playlist.add(new Musica("C#"));
+        playlist.add(new Musica("C 5e+"));
 
-        Album album = new Album("Programáticas", 2024, playlist);
+        Album album = new Album("C...", 2024, playlist);
 
         album.listar_album();
     }
